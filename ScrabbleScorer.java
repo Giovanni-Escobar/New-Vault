5fc8ac0a5b0f62f0f84da287cd4cdd8f6ce479d7
Giovanni-Escobar <70446136+Giovanni-Escobar@users.noreply.github.com>
2022-01-22 20:52:26 -0500

    Update ScrabbleScorer.java

--- ScrabbleScorer.java
+++ ScrabbleScorer.java
@@ -1,49 +1,72 @@
 import java.util.*;
 import java.io.*;
 
+/**
+ * ScrabbleScorer takes the user's input, verifies if it's a valid word in its dictionary,
+ * and scores the word for the user.
+ * @version Monday 1/24/22
+ * @author gescobar
+ */
 public class ScrabbleScorer {
     private ArrayList<String> dictionary;
     private int[] points = {1, 3, 3, 2, 1, 4, 2, 4, 1, 8, 5, 1, 3, 1, 1, 3, 10, 1, 1, 1, 1, 4, 4, 8, 4, 10};
     private String alpha;
 
-
-
+    /**
+     * Constructor for the class.
+     */
     public ScrabbleScorer(){
         dictionary = new ArrayList<>();
         alpha = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
     }
 
+    /**
+     * Reads the text file "SCRABBLE_WORDS.txt", adds it to dictionary,
+     * and then sorts it.
+     */
     public void buildDictionary(){
         try{
             Scanner in = new Scanner(new File("SCRABBLE_WORDS.txt"));
-            while(in.hasNextLine()){
+            while(in.hasNext()){
                 dictionary.add(in.nextLine());
             }
             in.close();
+            Collections.sort(dictionary);
         }
         catch (Exception e) {
             e.printStackTrace();
         }
-
     }
 
+    /**
+     * Searches through the array list "dictionary" and verifies whether
+     * the user's input is a valid word.
+     * @param word the user's input.
+     * @return returns negative numbers if the word is not in dictionary, and
+     * it returns positive numbers or "0" if it is.
+     */
     public boolean isValidWord(String word){
         return Collections.binarySearch(dictionary, word) >= 0;
     }
 
+    /**
+     * Calculates the amount of points for each of the user's words.
+     * @param word user's input.
+     * @return returns the amount of points the word is worth.
+     */
     public int getWordScore(String word){
         int sum = 0;
-
-        // parse word one letter at a time -- for loop, ""+charAt or substring
         for(int i = 0; i <= word.length()-1; i++){
             int num = points[alpha.indexOf(""+ word.charAt(i))];
             sum += num;
         }
-        // find the index of each letter
-        // add corresponding score/point value from points to sum value
-        return sum; // total number of points
+        return sum;
     }
 
+    /**
+     * The main method of the class ScrabbleScorer where it prints the user's score for their word.
+     * @param args
+     */
     public static void main(String[] args) {
         ScrabbleScorer app = new ScrabbleScorer();
         app.buildDictionary();
@@ -57,9 +80,11 @@ public class ScrabbleScorer {
                 if(userWord.equals("0"))
                     break;
                 else{
-                    // do the program
                     if(app.isValidWord(userWord.toUpperCase())){
-                        System.out.println(userWord.toUpperCase() + " = " + app.getWordScore(userWord.toUpperCase()) + " points" );
+                        System.out.println(userWord + " = " + app.getWordScore(userWord.toUpperCase()) + " points" );
+                    }
+                    else if(userWord.isBlank()){
+                        System.out.println("Please use a word");
                     }
                     else{
                         System.out.println(userWord.toUpperCase() + " is not a valid word in the dictionary");
@@ -73,7 +98,4 @@ public class ScrabbleScorer {
         }
         System.out.println("Exiting the program thanks for playing");
     }
-
-
-
 }
